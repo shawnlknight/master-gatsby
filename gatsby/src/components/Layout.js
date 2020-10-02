55ff093d4c9b36b31c43b732741a1fd4d444d78f
Shawn Leberknight <shawn.leberknight@somoglobal.com>
2020-10-02 11:02:57 -0400

    first commit

---
+++ gatsby/src/components/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from './Nav';
+import Footer from './Footer';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
+
+export default function Layout(props) {
+  const { children } = props;
+  return (
+    <div>
+      <GlobalStyles />
+      <Typography />
+      <Nav />
+      {children}
+      <Footer />
+    </div>
+  );
+}
---
+++ gatsby/src/pages/order.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function OrderPage() {
+  return (
+    <>
+      <p>Order Page</p>
+    </>
+  );
+}
