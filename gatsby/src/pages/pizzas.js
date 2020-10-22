c476ac0c9816f20bdb46583532ebd03e4ff2ba43
Shawn Leberknight <shawn.leberknight@somoglobal.com>
2020-10-22 16:19:19 -0400

    create PizzaList and display all pizzas on homepage

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+// Gatsby recognizes this graphql query and injects it magically into the page
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function PizzasPage({ data }) {
+  console.log(data);
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Pizzas Page</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
