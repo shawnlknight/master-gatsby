44957ac4ec59c17a3559758a1b86bb3747a217d1
Shawn Leberknight <shawn.leberknight@somoglobal.com>
2020-10-23 15:47:43 -0400

    style pizza grid

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -18,6 +18,9 @@ export const query = graphql`
         }
         image {
           asset {
+            fixed(width: 200, height: 200) {
+              ...GatsbySanityImageFixed
+            }
             fluid(maxWidth: 400) {
               ...GatsbySanityImageFluid
             }
@@ -29,7 +32,6 @@ export const query = graphql`
 `;
 
 export default function PizzasPage({ data }) {
-  console.log(data);
   const pizzas = data.pizzas.nodes;
   return (
     <>
