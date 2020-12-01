db04967d6d6cfdfee234fecd889dc1f71ad35588
Shawn Leberknight <shawn.leberknight@somoglobal.com>
2020-12-01 13:56:53 -0500

    style beers page

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import styled from 'styled-components';
 
-export default function BeersPage() {
+export const query = graphql`
+  query {
+    beers: allBeer {
+      nodes {
+        id
+        name
+        price
+        image
+        rating {
+          reviews
+          average
+        }
+      }
+    }
+  }
+`;
+
+const BeersGridStyles = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingleBeerStyles = styled.div`
+  border: 1px solid var(--grey);
+  padding: 2rem;
+  text-align: center;
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: contain;
+    display: block;
+    display: grid;
+    align-items: center;
+    font-size: 10px;
+  }
+`;
+
+export default function BeersPage({ data }) {
   return (
     <>
-      <p>Beers Page</p>
+      <h2 className="center" style={{ 'margin-bottom': '4rem' }}>
+        We have {data.beers.nodes.length} Beers Available. Dine in Only!
+      </h2>
+      <BeersGridStyles>
+        {data.beers.nodes.map((beer) => {
+          const rating = Math.round(beer.rating.average);
+          return (
+            <SingleBeerStyles key={beer.id}>
+              <img src={beer.image} alt={beer.name} />
+              <h3>{beer.name}</h3>
+              <h4>{beer.price}</h4>
+              <p title={`${rating} out of 5 stars`}>
+                {`⭐`.repeat(rating)}
+                <span
+                  style={{
+                    filter: `grayscale(100%)`,
+                  }}
+                >
+                  {`⭐`.repeat(5 - rating)}
+                </span>
+                <span>({beer.rating.reviews})</span>
+              </p>
+            </SingleBeerStyles>
+          );
+        })}
+      </BeersGridStyles>
     </>
   );
 }
